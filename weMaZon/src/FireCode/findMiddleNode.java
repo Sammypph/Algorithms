4ddc5311f7201bd140b5e4180c9285b47920d5ce
Oluwasanmi Akinrele <oakinrele@teamapt.com>
2020-04-08 10:34:03 +0100

    Added solution to LeetCode Challenge

--- weMaZon/src/FireCode/findMiddleNode.java
+++ weMaZon/src/FireCode/findMiddleNode.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class findMiddleNode
 {
 
-    public ListNode findMiddle(ListNode head)
+    public static ListNode findMiddle(ListNode head)
     {
       if(head == null)
       {
@@ -28,10 +28,42 @@ public class findMiddleNode
     }
 
 
-    class ListNode
+    public ListNode middleNode(ListNode head) {
+        //Get count of items in the LinkedList
+        int totalElements = 1;
+        ListNode snail = head;
+        ListNode heir = head;
+        while(heir != null && heir.next != null && heir.next.next != null)
+        {
+            snail = snail.next;
+            heir = heir.next.next;
+            totalElements += 2;
+        }
+        //Get total number of elements in the linkedList
+        if(heir.next != null){totalElements +=1;}
+        if(totalElements % 2 == 0)
+        {
+            snail = snail.next;
+        }
+        return snail;
+    }
+
+    static class ListNode
     {
         int data;
         ListNode next;
         ListNode(int data) { this.data = data; }
+    }
+
+    public static void main(String [] args)
+    {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        node.next.next.next.next.next = new ListNode(6);
+
+        System.out.println(findMiddle(node));
     }
 }
