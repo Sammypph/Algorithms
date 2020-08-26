410b3e3a7675c1c25be93d773cf2270ff4707702
Oluwasanmi Akinrele <oakinrele@teamapt.com>
2020-08-26 17:55:53 +0100

    String Compression solution, could be optimized but this is the initial solution

---
+++ weMaZon/src/hackerrank/StringCompression.java
@@ -0,0 +1,72 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by oakinrele on Aug, 2020
+ */
+public class StringCompression {
+
+    public static void compress(String s){
+        String input = s.toLowerCase();
+        //Result
+        StringBuilder result = new StringBuilder();
+        int length = input.length() - 1;
+        int occurrence = 1;
+
+        if(input != null && input.length() > 1) {
+            for (int i = 0; i < length ; i++) {
+
+                if(i < input.length() && input.charAt(i) != input.charAt(i+1)) {
+                    result.append(input.charAt(i));
+                    if(occurrence> 1)
+                        result.append(occurrence);
+                    //Set occurrence back to default
+                    occurrence = 1;
+                }
+
+                else {
+                    occurrence++;
+                }
+
+                if(i == (length - 1)){
+                    result.append(input.charAt(i+1));
+                    if(occurrence> 1)
+                        result.append(occurrence);
+                }
+
+            }
+        }
+
+        else
+        {
+            System.out.println(input);
+        }
+
+        System.out.println(result.toString());
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        List<String> allStrings = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        int inputCount = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 0; i < inputCount; i++)
+        {
+            String value = scanner.nextLine();
+            allStrings.add(value);
+        }
+
+
+        for(String s : allStrings)
+        {
+            compress(s);
+        }
+
+    }
+
+}
