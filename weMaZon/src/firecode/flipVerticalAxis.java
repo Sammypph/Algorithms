a5cd5f291ea72c15ced7bbe14a0f834e3213f734
Oluwasanmi Akinrele <oakinrele@teamapt.com>
2021-03-22 12:30:05 +0100

    Added another implementation for vertical flipping

--- weMaZon/src/firecode/flipVerticalAxis.java
+++ weMaZon/src/firecode/flipVerticalAxis.java
@@ -21,10 +21,31 @@ public class flipVerticalAxis {
     }
 
 
+    public static void flipItVerticalAxis2(int[][] matrix) {
+        flipIt(matrix);
+    }
+
+    private static void flipIt(int[][] matrix) {
+        int row = matrix.length - 1;
+        int column = matrix[0].length - 1;
+
+        for(int r = 0; r <= row; r++ ){
+            //Reset column
+            int col = column;
+            for(int c = 0; c <= col/2; c++){
+                int temp = matrix[r][c];
+                matrix[r][c] = matrix[r][col];
+                matrix[r][col] = temp;
+                col--;
+            }
+        }
+    }
+
+
     public static void main(String [] args){
         int [][] twoDimentional = {{1,0,1},{1,0,1},{1,0,1}};
         flipItVerticalAxis(twoDimentional);
 
-        String x = "Sanmki";
+        String x = "Sanmi";
     }
 }
